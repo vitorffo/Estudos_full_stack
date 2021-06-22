1619a7893cf8891d8b30f716d1172c7f3739d75f
vitor <vitor_fernando1604@hotmail.com>
2021-06-22 11:22:52 -0400

    Exercicio DOM e construção de pagina com loop

--- DOM/99lines_of_code_part2/app.js
+++ DOM/99lines_of_code_part2/app.js
@@ -1,11 +1,38 @@
+document.addEventListener('DOMContentLoaded', iniciaPagina);
 let friends = ['Juliano', 'Lucas', 'Maria', 'Ganem', 'Bruno'];
 
-for(let friend of friends){
-    for(i=99; i >= 1; i--){
-        if(i==1){
-            console.log(`${friend} ${i} linha de codigo`);
-            break;
-        }
-        console.log(`${friend} ${i} linhas de codigo`);
+
+function iniciaPagina(){
+    button = document.getElementById('sing');
+    button.addEventListener('click', canta);
+
+}
+
+function canta(){
+    
+    for(let friend of friends){
+        let div = document.createElement('div');
+        let dFriendText = document.createTextNode(friend);
+        let h3 = document.createElement('h3');
+        let paragraph = document.createElement('p');
+
+        div.className = 'friend';
+
+        h3.appendChild(dFriendText);
+        div.appendChild(h3);
+
+        for(let friend of friends){
+            for(i=99; i >= 1; i--){
+              if(i==1){
+                  paragraph.appendChild(document.createTextNode(`${friend} ${i} linha de codigo `));
+                  break;
+              }
+              paragraph.appendChild(document.createTextNode(`${friend} ${i} linhas de codigo `));
+         }
     }
+
+        div.appendChild(paragraph);
+        document.body.appendChild(div);
+    }
+
 }
